c5a590057de8afc2b872259ebd64f9caf2ec65a5
lukaszsl <lukasz.sliczner@interia.pl>
2018-04-07 10:58:07 +0000

    feat: Create Player class

--- js/app.js
+++ js/app.js
@@ -25,16 +25,36 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+class Player {
+	constructor() {
+		this.sprite = 'images/char-boy.png';
+	}
+
+	update(dt) {
+
+	}
+
+	render() {
+		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	}
+
+	handleInput() {
+		
+	}
+}
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-const enemy1 = new Enemy();
-const enemy2 = new Enemy();
-const enemy3 = new Enemy();
+let enemy1 = new Enemy();
+let enemy2 = new Enemy();
+let enemy3 = new Enemy();
 
 const allEnemies = [enemy1, enemy2, enemy3];
+
+let player = new Player;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
