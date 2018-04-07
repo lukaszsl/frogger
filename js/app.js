390bde2fe996bf72beb8b7a97335175f78e9f28c
lukaszsl <lukasz.sliczner@interia.pl>
2018-04-07 10:14:09 +0000

    Place all enemy objects in an array called allEnemies

--- js/app.js
+++ js/app.js
@@ -30,7 +30,11 @@ Enemy.prototype.render = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+const enemy1 = new Enemy();
+const enemy2 = new Enemy();
+const enemy3 = new Enemy();
 
+const allEnemies = [enemy1, enemy2, enemy3];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
