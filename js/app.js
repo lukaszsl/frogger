7785a75798e1d376ac33b1bca1ae6eaafe0fe7aa
lukaszsl <lukasz.sliczner@interia.pl>
2018-04-11 21:07:05 +0000

    style: Improve code comments

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 // Enemies our player must avoid
 class Enemy {
 	constructor(speedLimitValue = 'regular') {
-		// Variables applied to each of our instances go here
 		this.x = 0;
 		this.randomPositions = [60, 140, 220]; // possible enemy positions
-		this.y =  this.setRandomPosition(this.randomPositions.length); // random position
+		this.y =  this.setRandomPosition(this.randomPositions.length); // random position Y
 		this.speed = {
 			slow: 100,
 			regular: 200,
@@ -35,7 +34,7 @@ class Enemy {
 	}
 
 	// Set random initial position Y
-	// Function return one of the random possible positions
+	// Function return one of the random possible positions Y
 	setRandomPosition(max) {
 		return this.randomPositions[Math.floor(Math.random() * Math.floor(max))]; // return number from 0 to max
 	}
@@ -43,11 +42,11 @@ class Enemy {
 	// Update the enemy's position, required method for game
 	// Parameter: dt, a time delta between ticks
 	update(dt) {
-		// You should multiply any movement by the dt parameter
+		// Multiply any movement by the dt parameter
 		// which will ensure the game runs at the same speed for
 		// all computers.
 		this.x += this.speedLimit * dt;
-		// If enemy hit the end of the board, reset positions
+		// If enemy reaches the end of the board, reset positions
 		if (this.x > 505) {
 			this.x = 0;
 			this.y = this.setRandomPosition(this.randomPositions.length);
@@ -60,10 +59,8 @@ class Enemy {
 	}
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
+// Player
 class Player {
 	constructor() {
 		this.sprite = 'images/char-boy.png'; // Load the player image
@@ -88,6 +85,7 @@ class Player {
 		}
 	}
 
+	// Draw the player on the screen, required method for game
 	render() {
 		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 	}
@@ -125,27 +123,22 @@ class Player {
 }
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-let enemy1 = new Enemy('slow');
-let enemy2 = new Enemy('regular');
-let enemy3 = new Enemy('fast');
-
+const enemy1 = new Enemy('slow');
+const enemy2 = new Enemy('regular');
+const enemy3 = new Enemy('fast');
 
 const allEnemies = [enemy1, enemy2, enemy3];
 
-let player = new Player();
+const player = new Player();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Listen for key presses and send the keys to
+// Player.handleInput() method
 document.addEventListener('keyup', function(e) {
-		var allowedKeys = {
-				37: 'left',
-				38: 'up',
-				39: 'right',
-				40: 'down'
-		};
-		player.handleInput(allowedKeys[e.keyCode]);
+	var allowedKeys = {
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down'
+	};
+	player.handleInput(allowedKeys[e.keyCode]);
 });
