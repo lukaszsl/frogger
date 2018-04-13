30a60d52e5979874d0a925318eab89390a705255
lukaszsl <lukasz.sliczner@interia.pl>
2018-04-13 21:22:23 +0000

    feat: Append canvas to the board container

--- js/engine.js
+++ js/engine.js
@@ -21,12 +21,13 @@ var Engine = (function(global) {
 		var doc = global.document,
 				win = global.window,
 				canvas = doc.createElement('canvas'),
+				boardContainer = document.querySelector('.board-container'),
 				ctx = canvas.getContext('2d'),
 				lastTime;
 
 		canvas.width = 505;
 		canvas.height = 606;
-		doc.body.appendChild(canvas);
+		boardContainer.appendChild(canvas);
 
 		/* This function serves as the kickoff point for the game loop itself
 		 * and handles properly calling the update and render methods.
